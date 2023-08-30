8f4e700d9faf040a954f1bcea0139e8e09df0c6a
Arik Mukherjee <112742649+arikmukherjee@users.noreply.github.com>
2023-08-30 13:07:28 +0530

    Add files via upload

---
+++ Package in java/Package-01/Package1.java
@@ -0,0 +1,12 @@
+package p1arik;
+
+public class Package1 {
+    void m1()
+    {
+        System.out.println("I am method m1");
+    }
+    public static void main(String[] args) {
+        Package1 obj=new Package1();
+        obj.m1();
+    }
+}
---
+++ Package in java/Package-02/Package2.java
@@ -0,0 +1,8 @@
+package p1arik.p2mukherjee;
+
+class Package2
+{
+    public static void main(String[] args) {
+        System.out.println("hello package");
+    }
+}
