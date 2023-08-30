9c44ec87ca84aab8a14257d9ce9a0584190fa566
Arik Mukherjee <112742649+arikmukherjee@users.noreply.github.com>
2023-08-30 19:52:37 +0530

    Add files via upload

---
+++ Wrapper class/W1.java
@@ -0,0 +1,15 @@
+class W1
+{
+    public static void main(String[] args) {
+        Integer ob=100;
+        int i=ob;
+        System.out.println(ob);
+        System.out.println(i);
+
+        Integer obj=new Integer(500);
+        int j=obj;
+
+        System.out.println(j);
+        System.out.println(obj);
+    }
+}
